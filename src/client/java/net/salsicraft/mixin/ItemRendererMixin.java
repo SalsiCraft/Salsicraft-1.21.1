a46f1c0ce70641eac251b96b0922667ec30107a7
Vinicius Leal Alves <59040683+lealalves@users.noreply.github.com>
2025-12-17 01:59:48 -0300

    feature: adiciona modelo personalizado fragmento de etherita

---
+++ src/client/java/net/salsicraft/mixin/ItemRendererMixin.java
@@ -0,0 +1,54 @@
+package net.salsicraft.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.salsicraft.Salsicraft;
+import net.salsicraft.ores.etherite.EtheriteItems;
+import net.minecraft.client.render.item.ItemModels;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+
+    @Shadow
+    @Final
+    private ItemModels models;
+
+    @Shadow
+    public abstract ItemModels getModels();
+
+    @ModifyVariable(
+            method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+            at = @At(value = "HEAD"),
+            argsOnly = true
+    )
+    public BakedModel renderItem(BakedModel bakedModel, @Local(argsOnly = true) ItemStack stack, @Local(argsOnly = true) ModelTransformationMode renderMode) {
+        if (stack.getItem() == EtheriteItems.ETHERITE_FRAGMENT && (renderMode == ModelTransformationMode.GUI || renderMode == ModelTransformationMode.FIXED)) {
+            return getModels().getModelManager().getModel(ModelIdentifier.ofInventoryVariant(Identifier.of(Salsicraft.MOD_ID, "etherite_fragment")));
+        }
+
+        return bakedModel;
+    }
+
+    @ModifyVariable(
+            method = "getModel",
+            at = @At(value = "STORE"),
+            ordinal = 1
+    )
+    public BakedModel getHeldItemModelMixin(BakedModel bakedModel, @Local(argsOnly = true) ItemStack stack) {
+        if (stack.getItem() == EtheriteItems.ETHERITE_FRAGMENT) {
+            return this.models.getModelManager().getModel(ModelIdentifier.ofInventoryVariant(Identifier.of(Salsicraft.MOD_ID, "etherite_fragment_3d")));
+        }
+
+        return bakedModel;
+    }
+}
